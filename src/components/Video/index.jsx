82fc564d383221649a34b410de3bb9ea05c49678
Ajain Vivek <ajainvivek07@gmail.com>
2021-04-12 11:18:04 +1000

    feat: get stats + display multiple video stream

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,75 +1,111 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState, useEffect, useRef,
+} from 'react';
 import Peer from 'simple-peer';
+import { v4 as uuid } from 'uuid';
+import { filter, find } from 'lodash';
 
 import './style.css';
 
 const Video = () => {
-  const [message, setMessage] = useState(null);
-  const video1 = useRef(null);
-  const video2 = useRef(null);
-  const peer1 = new Peer({
-    initiator: true,
-    config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }, { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }] },
-  });
-  const peer2 = new Peer();
+  const videoRef = useRef();
+  // host connection
+  const hostConnection = {
+    id: uuid(),
+    peer: new Peer({
+      initiator: true,
+      config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] },
+    }),
+    video: useRef(null),
+  };
+  // peer connection
+  const peerConnection = {
+    id: uuid(),
+    peer: new Peer({
+      config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] },
+    }),
+    video: useRef(null),
+  };
+
+  const [connections] = useState([hostConnection, peerConnection]);
 
   // load media stream
-  const loadMediaStream = async (peer) => {
+  const loadMediaStream = async (connection) => {
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true,
       });
-      peer.addStream(mediaStream);
+      connection.peer.addStream(mediaStream);
     } catch (err) {
-      setMessage('Failed loading stream...');
+      // TODO: Need to display the message inline to video element
+      console.log('Failed loading stream...');
     }
+  };
+
+  // get peer stats
+  const getBandwidthStats = ({ peer }) => {
+    peer.getStats((_, reports) => {
+      // TODO: Display the report in DOM
+      const transport = find(reports, { type: 'transport' });
+      console.log(transport);
+    });
+  };
+
+  // bind peer connections
+  const bindPeer = (connection) => {
+    const { peer, video } = connection;
+    const signalConnections = filter(connections, (conn) => conn.id !== connection.id);
+
+    // signal to other peer to perform handshake
+    peer.on('signal', (data) => {
+      signalConnections.forEach((conn) => {
+        conn.peer.signal(data);
+      });
+    });
+
+    // load the stream to video
+    peer.on('stream', (stream) => {
+      if ('srcObject' in video.current) {
+        video.current.srcObject = stream;
+      } else {
+        video.current.src = window.URL.createObjectURL(stream); // for older browsers
+      }
+      video.current.play();
+    });
   };
 
   // on page load get user media
   useEffect(() => {
-    loadMediaStream(peer1);
-    loadMediaStream(peer2);
-  }, []);
-
-  peer1.on('signal', (data) => {
-    peer2.signal(data);
-  });
-
-  peer2.on('signal', (data) => {
-    peer1.signal(data);
-  });
-
-  peer1.on('stream', (stream) => {
-    if ('srcObject' in video1.current) {
-      video1.current.srcObject = stream;
-    } else {
-      video1.current.src = window.URL.createObjectURL(stream); // for older browsers
-    }
-    video1.current.play();
-  });
-
-  peer2.on('stream', (stream) => {
-    if ('srcObject' in video2.current) {
-      video2.current.srcObject = stream;
-    } else {
-      video2.current.src = window.URL.createObjectURL(stream); // for older browsers
-    }
-    video2.current.play();
+    connections.forEach((connection) => {
+      loadMediaStream(connection);
+      bindPeer(connection);
+    });
   });
 
   return (
-    <div className="Video">
-      {!message ? (
-        <video ref={video1} height="500" width="500">
-          <track kind="captions" />
-        </video>
-      ) : message}
-      {!message ? (
-        <video ref={video2} height="500" width="500">
-          <track kind="captions" />
-        </video>
-      ) : message}
+    <div ref={videoRef} className="VideoWrapper">
+      {connections.map((connection) => {
+        const { video, id } = connection;
+        return (
+          <div key={`${id}-video-wrapper`}>
+            <video key={`${id}-video`} ref={video} className="Video">
+              <track key={`${id}-track`} kind="captions" />
+            </video>
+            <button
+              key={`${id}-stats`}
+              type="submit"
+              onClick={() => {
+                getBandwidthStats(connection);
+              }}
+            >
+              Show Peer (
+              {`${connection.id}`}
+              ) Stats - Check Console
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
